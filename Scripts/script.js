0d59ba95b7f1ac762f11cdc772316fede0d4cc25
Henrique Sousa Brito <henriquesousa5507@gmail.com>
2023-02-11 23:25:40 -0300

    Script adcionado

---
+++ Scripts/script.js
@@ -0,0 +1,32 @@
+let aside = window.document.getElementById('aside')
+let iphone = window.document.getElementById('iphone') 
+
+function verde(){
+    aside.style.backgroundColor ='rgb(33, 68, 33)'
+    document.getElementById('iphone').src = "imagens/iphone/green.png"
+}
+
+function rosa(){
+    aside.style.backgroundColor = 'rgb(150, 74, 153)'
+    document.getElementById('iphone').src = "imagens/iphone/pink.png"
+}
+
+function azul(){
+    aside.style.backgroundColor = 'rgb(32, 108, 196)'
+    document.getElementById('iphone').src = "imagens/iphone/blue.png"
+}
+
+function preto(){
+    aside.style.backgroundColor = 'rgb(0, 0, 0)'
+    document.getElementById('iphone').src = "imagens/iphone/black.png"
+}
+
+function branco(){
+    aside.style.backgroundColor = 'rgb(155, 152, 152)'
+    document.getElementById('iphone').src = "imagens/iphone/white.png"
+}
+
+function vermelho(){
+    aside.style.backgroundColor = 'rgb(211, 22, 69)'
+    document.getElementById('iphone').src = "imagens/iphone/red.png"
+}
